f410b6aeb9195f4f2e20d19512d1659abe2b1d77
hendrikwo2000 <116956869+hendrikwo2000@users.noreply.github.com>
2022-11-30 18:18:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,55 @@
-let darkmode;
-
-
-
-/*if(window.matchMedia('prefers-color-scheme: dark)').matches){
-	darkmode = true;
-	dakmode()
-}else{
-	darkmode = false;
-	darkmode();
-}
-*/
-
-if(darkmode == true){dakmode()};
 
 
 
 
-function switchmode(){ 
-	console.log(darkmode);
-	darkmode = !darkmode;
+var modee = "false";
+modee = window.localStorage.getItem("thema");
+console.log(modee);
+test();
 
-	if (darkmode == true){
-		dakmode();
 
-}else{
+
+
+function test(){
+if(modee == "true"){
+	console.log(modee + " true");
+	dark();
+}else{ (modee == "false")
+	console.log(modee + " false");
 	lightmode();
 }
 
-console.log(darkmode);
+};
+
+function switchmode(){ 
+	
+
+	if (modee == "true"){
+		modee = "false";
+		light();
+}else{
+	modee = "true";
+	
+		dark();
+}
 
 
-
+localStorage.setItem("thema" , modee);
 };
 
 
 
 
-function dakmode(){
-	console.log("dakmode is true = "+ darkmode);
+
+
+function dark(){
 	document.getElementById("nav").className = "navbar navbar-expand-lg bg-dark";
-	document.getElementById("it-wolf").src = "it-wolf-weis.svg";
-	document.getElementById("mode").src = "sun.svg";
+	document.getElementById("it-wolf").src = "Bilder/it-wolf-weis.svg";
+	document.getElementById("mode").src = "Bilder/mode-sun.svg";
 	document.getElementById("body").className = "dark-mode";
 	document.getElementById("link1").className = "dark-mode";
 	document.getElementById("link2").className = "dark-mode";
+	
 	document.getElementById("card1").style.background = "#202020";
 	document.getElementById("card2").style.background = "#202020";
 	document.getElementById("card3").style.background = "#202020";
@@ -54,15 +60,15 @@ function dakmode(){
 	document.getElementById("drop2").style.color = "white";
 	document.getElementById("drop3").style.color = "white";
 	document.getElementById("drop").style.background ="#202020";
+	
 
 }
 
 
-function lightmode(){
-	console.log("dakmode is false = "+ darkmode);
+function light(){
 	document.getElementById("nav").className = "navbar navbar-expand-lg bg-light";
-	document.getElementById("it-wolf").src = "it-wolf-schwarz.svg";
-	document.getElementById("mode").src = "moon.svg";
+	document.getElementById("it-wolf").src = "Bilder/it-wolf-schwarz.svg";
+	document.getElementById("mode").src = "Bilder/mode-moon.svg";
 	document.getElementById("body").className = "light-mode";
 	document.getElementById("link1").className = "light-mode";
 	document.getElementById("link2").className = "light-mode";
@@ -76,6 +82,7 @@ function lightmode(){
 	document.getElementById("drop2").style.color = "black";
 	document.getElementById("drop3").style.color = "black";
 	document.getElementById("drop").style.background ="white";
+	
 
 }
 
