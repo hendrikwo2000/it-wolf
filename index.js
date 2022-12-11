2573e2531293ea2686d667aa2cdf2ad1b077d2c4
hendrikwo2000 <116956869+hendrikwo2000@users.noreply.github.com>
2022-12-11 20:30:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
 
+let imphover = "false";
+let tip = "false";
+function imp(){
+	imphover = "true";
+if(tip == "true"){
+	location.href = 'secret.html';
 
-const halt = new Audio('Sounds/halt_stop.mp3');
-const hurt = new Audio('Sounds/hurt.mp3');
-const na = new Audio('Sounds/na.mp3');
+}
+	imphover = "false";
+	tip = "false";
 
-
-document.onkeydown = function (event) {
-	if (event.keyCode == 32) {
-		halt.play();
-	}
-
-	if (event.keyCode == 46) {
-		hurt.play();
-	}
-	if(event.keyCode == 8) {
-		hurt.play();
-	}
-	if (event.keyCode == 13) {
-		na.play();
-	}
 };
+
+function tippen(){
+	tip = "true";
+};
+
+
+
+
+
+
 
 
 var modee = "false";
 modee = window.localStorage.getItem("thema");
 console.log(modee);
+modestart();
+
+
+function modestart() {
+
+
+	if (window.localStorage.getItem("thema") == "true") {
+		
+		//alert("Keine Ahnung warum der Darkmode nicht mehr an geht");
+		dark2();
+	} };
+
+
+function dark2() {
+	console.log("dark2 ausgelöst");
+	switchmode();
+
+
+}
+
+
+
 
 
 function switchmode(){ 
@@ -38,12 +61,8 @@ function switchmode(){
 	
 		dark();
 }
-
-
 localStorage.setItem("thema" , modee);
 };
-
-
 
 
 
@@ -55,17 +74,14 @@ function dark(){
 	document.getElementById("body").className = "dark-mode";
 	document.getElementById("link1").className = "dark-mode";
 	document.getElementById("link2").className = "dark-mode";
-	
 	document.getElementById("card1").style.background = "#202020";
 	document.getElementById("card2").style.background = "#202020";
 	document.getElementById("card3").style.background = "#202020";
 	document.getElementById("nav1").style.color = "white";
 	document.getElementById("nav2").style.color = "white";
 	document.getElementById("nav3").style.color = "white";
-	
 
 };
-
 
 function light(){
 	document.getElementById("nav").className = "navbar navbar-expand-lg bg-light";
@@ -81,29 +97,5 @@ function light(){
 	document.getElementById("nav2").style.color = "black";
 	document.getElementById("nav3").style.color = "black";
 
-	
-
 };
 
-function card_1 (){
-	const thud = new Audio('Sounds/thud.mp3');
-	thud.play();
-	
-};
-
-function card_2() {
-	const entscheidung = new Audio('Sounds/entscheidung.mp3');
-	entscheidung.play();
-
-};
-
-function card_3() {
-	const moment = new Audio('Sounds/moment.mp3');
-	moment.play();
-
-};
-
-function rick(){
-	const rick = new Audio('Sounds/rick.mp3');
-	rick.play();
-}
