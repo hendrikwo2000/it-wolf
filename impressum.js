90ddeaa6a128387a790cf3256ca3f59b2b583478
hendrikwo2000 <116956869+hendrikwo2000@users.noreply.github.com>
2022-12-17 17:28:16 +0100

    Update impressum.js

--- impressum.js
+++ impressum.js
@@ -4,7 +4,7 @@ let tip = "false";
 function imp() {
     imphover = "true";
     if (tip == "true") {
-        location.href = 'secret.html';
+        location.href = 'sounds.html';
 
     }
     imphover = "false";
@@ -16,24 +16,19 @@ function tippen() {
     tip = "true";
 };
 
-
-function card_1() {
-    const thud = new Audio('Sounds/thud.mp3');
-    thud.play();
+function card_2() {
+    const schlag = new Audio('Sounds/schlag.mp3');
+    schlag.play();
 
 };
 
-function card_2() {
+function card_3() {
     const entscheidung = new Audio('Sounds/entscheidung.mp3');
     entscheidung.play();
 
 };
 
-function card_3() {
-    const moment = new Audio('Sounds/moment.mp3');
-    moment.play();
 
-};
 
 function rick() {
     const rick = new Audio('Sounds/rick.mp3');
