187e0676b4562e4be03e754d0176e4c05fe7b9b1
hendrikwo2000 <116956869+hendrikwo2000@users.noreply.github.com>
2023-01-28 20:30:00 +0100

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,3 +1,12 @@
+
+
+
+
+
+
+
+
+
 const halt = new Audio('Sounds/halt_stop.mp3');
 const hurt = new Audio('Sounds/hurt.mp3');
 const na = new Audio('Sounds/na.mp3');
@@ -11,6 +20,8 @@ const n6 = new Audio('Sounds/n6.mp3');
 const alarm = new Audio('Sounds/alarm.mp3');
 const anzeige = new Audio('Sounds/anzeige.mp3');
 const wichser = new Audio('Sounds/wichser.mp3');
+
+
 
 document.onkeydown = function (event) {
 
@@ -72,4 +83,12 @@ function fehler(){
 function rick() {
     const rick = new Audio('Sounds/rick.mp3');
     rick.play();
+
+    document.getElementById("butt").disabled = true;
+   
+    setTimeout(function () {
+        document.getElementById("butt").disabled = false;
+    }, 17000);
+    
+
 };
