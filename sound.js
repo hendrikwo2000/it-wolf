4d2e2a8ec9a9df4d82b2a6745b1dca3ab9db6016
hendrikwo2000 <116956869+hendrikwo2000@users.noreply.github.com>
2023-02-11 17:44:25 +0100

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,77 +1,86 @@
 
-
-
-
-
-
-
-
-
-const halt = new Audio('Sounds/halt_stop.mp3');
-const hurt = new Audio('Sounds/hurt.mp3');
-const na = new Audio('Sounds/na.mp3');
-const deutschland = new Audio('Sounds/deutschland.mp3');
-const sui = new Audio('Sounds/sui.mp3');
-const krise = new Audio('Sounds/krise.mp3');
-const me = new Audio('Sounds/2022.mp3');
-const schwanz = new Audio('Sounds/schwanz.mp3');
-const erika = new Audio('Sounds/erika.mp3');
-const n6 = new Audio('Sounds/n6.mp3');
-const alarm = new Audio('Sounds/alarm.mp3');
-const anzeige = new Audio('Sounds/anzeige.mp3');
 const wichser = new Audio('Sounds/wichser.mp3');
 
 
 
 document.onkeydown = function (event) {
 
-
-    if (event.keyCode == 54) {
-        n6.play();
-    }
-    if (event.keyCode == 32) {
-        halt.play();
-    }
-    if (event.keyCode == 46) {
-        hurt.play();
-    }
-    if (event.keyCode == 8) {
-        hurt.play();
-    }
-    if (event.keyCode == 13) {
-        na.play();
-    }
-    if (event.keyCode == 49) {
-        deutschland.play();
-    }
-    if (event.keyCode == 50) {
-        me.play();
-    }
-    if (event.keyCode == 51) {
-        sui.play();
-    }
-    if (event.keyCode == 52) {
-        krise.play();
-    }
-    if (event.keyCode == 53) {
-        schwanz.play();
-    }
-    if (event.keyCode == 54) {
-        n6.play();
-    }
-    if (event.keyCode == 55) {
-        erika.play();
-    }
-    if (event.keyCode == 56) {
-        alarm.play();
-    }
-    if (event.keyCode == 57) {
-        anzeige.play();
-    }
     if (event.keyCode == 48) {
         wichser.play();
     }
 };
+
+function deutschalnd(){
+    const deutschalnd = new Audio('Sounds/deutschland.mp3');
+    deutschalnd.play();
+}
+
+
+function wirhaben() {
+    const wirhaben = new Audio('Sounds/2022.mp3');
+    wirhaben.play();
+}
+
+
+function sui() {
+    const sui = new Audio('Sounds/sui.mp3');
+    sui.play();
+}
+
+
+function krise() {
+    const krise = new Audio('Sounds/krise.mp3');
+    krise.play();
+}
+
+
+function schwanz() {
+    const schwanz = new Audio('Sounds/schwanz.mp3');
+    schwanz.play();
+}
+
+
+function n6() {
+    const n6 = new Audio('Sounds/n6.mp3');
+    n6.play();
+}
+
+
+function erika() {
+    const erika = new Audio('Sounds/erika.mp3');
+    erika.play();
+}
+
+
+function alarm() {
+    const alarm = new Audio('Sounds/alarm.mp3');
+    alarm.play();
+}
+
+
+function anzeige() {
+    const anzeige = new Audio('Sounds/anzeige.mp3');
+    anzeige.play();
+}
+
+
+function hurt() {
+    const hurt = new Audio('Sounds/hurt.mp3');
+    hurt.play();
+}
+
+
+function na() {
+    const na = new Audio('Sounds/na.mp3');
+    na.play();
+}
+
+
+function halt_stop() {
+    const halt_stop = new Audio('Sounds/halt_stop.mp3');
+    halt_stop.play();
+}
+
 
 
 function fehler(){
